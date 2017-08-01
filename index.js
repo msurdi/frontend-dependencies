26fe3195aefc233b23e8235f4a6f0b36e20fe33b
Ian Turgeon <iturgeon@gmail.com>
2017-08-01 15:08:11 -0400

    allow multiple targets and explicit target paths

--- index.js
+++ index.js
@@ -22,31 +22,59 @@ function frontendDependencies(workDir) {
         fail("No 'frontendDependencies' key in package.json");
     }
 
-    if (!packageJson.frontendDependencies.target) {
-        fail("No 'frontendDependencies.target' key in package.json");
-    }
-
     if (!packageJson.frontendDependencies.packages) {
         fail("No 'frontendDependencies.packages' key in package.json");
     }
 
     var packages = packageJson.frontendDependencies.packages || [];
     for (var i = 0; i < packages.length; i++) {
-        var packageName = packages[i].split("/").slice(0, 1)[0];
-        var desiredFilesGlob = packages[i].split("/").slice(1).join("/") || "/*";
+        // set defaults
+        var targetPath = null;
+        var exact = false;
+        var entry = packages[i];
+
+        // new optional syntax that lets each entry have a different target
+        if (typeof entry === "object") {
+            if(!entry.hasOwnProperty('src')) {
+                fail("Entry missing src key");
+            }
+
+            if(entry.hasOwnProperty('target')) {
+                targetPath = entry.target
+            }
+
+            exact = entry.hasOwnProperty('exact') && entry.exact == true;
+            entry = entry.src;
+        }
+
+        // set a default target if not set via object
+        if (!targetPath) {
+            if(!packageJson.frontendDependencies.target) {
+                fail("No 'frontendDependencies.target' key in package.json");
+            }
+            targetPath = packageJson.frontendDependencies.target;
+        }
+
+
+        var packageName = entry.split("/").slice(0, 1)[0];
+        var desiredFilesGlob = entry.split("/").slice(1).join("/") || "/*";
         var modulePath = path.join(workDir, "node_modules", packageName);
         var desiredFilesPath = path.join(modulePath, desiredFilesGlob);
-        var targetPath = path.join(workDir, packageJson.frontendDependencies.target, packageName);
+        targetPath = path.join(workDir, targetPath);
 
         if (!shell.test("-d", modulePath)) {
             fail("Module not found or not a directory: " + modulePath);
         }
 
-        shell.mkdir("-p", targetPath);
-        shell.cp("-r", desiredFilesPath, targetPath);
+        if (exact) {
+            shell.mkdir("-p", path.dirname(targetPath));
+            shell.cp("-r", desiredFilesPath, targetPath);
+        } else {
+            targetPath = path.join(targetPath, packageName);
+            shell.mkdir("-p", targetPath);
+            shell.cp("-r", desiredFilesPath, targetPath);
+        }
     }
-
-
 }
 
 function fail(reason) {
